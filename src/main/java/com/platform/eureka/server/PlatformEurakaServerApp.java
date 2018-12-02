59490f3f50769a826f5cd1810d3e7f5634043005
kangjin.zhao <kangjin.zhao@zkjstar>
2018-12-02 21:34:51 +0800

    服务发现

--- src/main/java/com/platform/eureka/server/PlatformEurakaServerApp.java
+++ src/main/java/com/platform/eureka/server/PlatformEurakaServerApp.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
-//@ComponentScan(basePackages={"com.platflat.eureka.server"})
-//@MapperScan("com.platflat.eureka.server.dao")
-//@EnableCaching
 @EnableEurekaServer
 public class PlatformEurakaServerApp {
 	public static void main(String[] args) {
